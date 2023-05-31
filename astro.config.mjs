a3e668c8468ef42e92f4b90585b065540df3a5a7
ossinova <oscar.dyremyhr@gmail.com>
2023-06-01 00:04:52 +0200

    Removed mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config';; 
 import sitemap from '@astrojs/sitemap';
-
-import react from "@astrojs/react";
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()]
+	site: 'https://example.com',
+	integrations: [sitemap(), react()],
 });
